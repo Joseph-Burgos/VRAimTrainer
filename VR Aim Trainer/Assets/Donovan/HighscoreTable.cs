aa3c21414e8088a02d51c24bd5d7f02c969cd269
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-16 15:34:45 -0700

    Insert scores from database into the scoreboard

--- VR Aim Trainer/Assets/Donovan/HighscoreTable.cs
+++ VR Aim Trainer/Assets/Donovan/HighscoreTable.cs
@@ -15,7 +15,7 @@ public class HighscoreTable : MonoBehaviour
     {
         Debug.Log("Starting the leaderboard retrieval process");
         client = new HttpClient();
-        string responseString = await client.GetStringAsync("http://localhost:3456/scores?topScores=2");
+        string responseString = await client.GetStringAsync("http://localhost:3456/scores?topScores=5"); // TODO handle no connection
 
         Debug.Log("original response");
         Debug.Log(responseString);
@@ -26,13 +26,15 @@ public class HighscoreTable : MonoBehaviour
         Debug.Log("Retrieved objects from server successfully");
         Debug.Log(sb.ToString());
 
-        Debug.Log("Successfully loaded the leaderboard");
+        List<Score> highScores = sb.scores;
+
+        //Debug.Log("Successfully loaded the leaderboard");
         entryContainer = transform.Find("highscoreEntryContainer");
         entryTemplate = entryContainer.Find("highscoreEntryTemplate");
         entryTemplate.gameObject.SetActive(false);
 
         float templateHeight = 20f;
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < highScores.Count; i++)
         {
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
@@ -49,10 +51,14 @@ public class HighscoreTable : MonoBehaviour
                 case 3: rankString = "3rd"; break;
             }
 
+            Score currentScore = highScores[i];
+
             entryTransform.Find("postText").GetComponent<Text>().text = rankString;
-            int score = Random.Range(0, 10000);
+            int score = currentScore.points;
             entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
-            string name = "AAA";
+            string mode = currentScore.gameMode;
+            entryTransform.Find("gameText").GetComponent<Text>().text = mode.ToString();
+            string name = currentScore.userName;
             entryTransform.Find("nameText").GetComponent<Text>().text = name;
         }
     }
@@ -78,6 +84,6 @@ public class Scoreboard
 public class Score {
     public string userName;
     public string gameMode;
-    public string points;
+    public int points;
     public string date;
 }
