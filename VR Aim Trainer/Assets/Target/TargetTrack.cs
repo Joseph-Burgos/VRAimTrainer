feff854c279c87364f2949d00280e117b167301c
Donovan Lee <donovan10599@gmail.com>
2021-09-19 02:29:31 -0700

    Tracking Now works, able to get average percentage in it as well, and make game stop at a certain time. :D

--- VR Aim Trainer/Assets/Target/TargetTrack.cs
+++ VR Aim Trainer/Assets/Target/TargetTrack.cs
@@ -6,7 +6,19 @@ public class TargetTrack : Target_Parent
 {
     public float hitCount = 3.0f;
     public float totalCount = 3.0f;
-    
+
+    public Transform startPos, endPos;
+    public bool repeatable = false;
+    public float speed = 1.0f;
+    float startTime, totalDistance;
+    IEnumerator Start(){
+        startTime = Time.time;
+        totalDistance = Vector3.Distance(startPos.position, endPos.position);
+        while(repeatable){
+            yield return RepeatLerp(startPos.position, endPos.position, 3.0f);
+            yield return RepeatLerp(endPos.position, startPos.position, 3.0f);
+        }
+    }
 
 
     public override void hit(){
@@ -16,15 +28,31 @@ public class TargetTrack : Target_Parent
 
     void Update()
     {
-        
-        if(totalCount >= 20){
-            Time.timeScale = 0;
-            float timer = hitCount/totalCount;
-            Debug.Log("Accuracy is ", timer);
+        if(!repeatable){
+            float currentDuration = (Time.time - startTime) * speed;
+            float journeyFraction = currentDuration / totalDistance;
+            this.transform.position = Vector3.Lerp(startPos.position, endPos.position, journeyFraction);
         }
         totalCount+= Time.deltaTime;
-        //Debug.Log("Total Timer: " + totalCount);
-        
 
+        if(totalCount >= 20){
+            StopGame();
+            totalCount = 0;
+        }
     }
+    public void StopGame(){
+        float avg = hitCount / totalCount;
+        Debug.Log("Average: " + (avg * 100));
+        Time.timeScale = 0;
+    }
+    public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time){
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f){
+            i += Time.deltaTime * rate;
+            this.transform.position = Vector3.Lerp(a ,b , i);
+            yield return null;
+        }
+    }
+    
 }
