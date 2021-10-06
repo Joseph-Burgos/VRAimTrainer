3ea94da663d01b58bf838b7f4db9b6f8e9acade8
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-06 14:33:09 -0700

    Removes unused lines (leaves debug statements)

--- VR Aim Trainer/Assets/ScoreManager.cs
+++ VR Aim Trainer/Assets/ScoreManager.cs
@@ -1,38 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using System;
-using TMPro;
-
-using UnityEngine.UI;
-
 
 public class ScoreManager : MonoBehaviour
 {
     private int m_Score;
     private int num_calls;
-
-    private Transform scoreContainer; // parent of current score display
-    private Transform scoreTMP; // object that has the text for the current score
-    private TextMeshProUGUI z;
-
     private GameObject CurrentScore;
 
     void Start(){
         Debug.Log("Start - enter");
-        //CurrentScore = GameObject.Find("CurrentScore");
-        num_calls = 1;
+        CurrentScore = GameObject.Find("Score");
+        num_calls = 2;
+        // initialize scoreboard to 0
         m_Score = 0;
-        //Debug.Log("Start - Current score object " + CurrentScore.ToString());
-
-        scoreContainer = transform.Find("Score");
-        scoreContainer.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
-
-        Debug.Log("Start - found current score container " + scoreContainer.ToString());
+        CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
         Debug.Log("Start - exit");
     }
 
-    private void Update()
+    // Update tests the AddToScore function
+    private void Update() 
     {
         if (num_calls > 0) {
             Debug.Log("Update - enter");
@@ -44,12 +31,9 @@ public class ScoreManager : MonoBehaviour
 
     public void AddToScore(int scoreToBeAdded){
         Debug.Log("AddToScore - enter");
-
-
         if (scoreToBeAdded > 0){
             m_Score += scoreToBeAdded;
-            //CurrentScore = transform.Find("Score");
-            scoreContainer.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
+            CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
         }
         Debug.Log("AddToScore - current score is " + m_Score.ToString());
         Debug.Log("AddToScore - exit");
