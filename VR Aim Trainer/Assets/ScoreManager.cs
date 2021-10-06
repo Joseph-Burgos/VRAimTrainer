5a6622fd369f9c8150b83d383169027f99e1567a
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-06 14:07:11 -0700

    Access and change the current score display from the CurrentScore object

--- VR Aim Trainer/Assets/ScoreManager.cs
+++ VR Aim Trainer/Assets/ScoreManager.cs
@@ -2,25 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
+
+using UnityEngine.UI;
+
 
 public class ScoreManager : MonoBehaviour
 {
     private int m_Score;
+    private int num_calls;
+
+    private Transform scoreContainer; // parent of current score display
+    private Transform scoreTMP; // object that has the text for the current score
+    private TextMeshProUGUI z;
 
     private GameObject CurrentScore;
 
     void Start(){
-        CurrentScore = GameObject.Find("ENVIRONMENT");
+        Debug.Log("Start - enter");
+        //CurrentScore = GameObject.Find("CurrentScore");
+        num_calls = 1;
+        m_Score = 0;
+        //Debug.Log("Start - Current score object " + CurrentScore.ToString());
+
+        scoreContainer = transform.Find("Score");
+        scoreContainer.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
+
+        Debug.Log("Start - found current score container " + scoreContainer.ToString());
+        Debug.Log("Start - exit");
+    }
+
+    private void Update()
+    {
+        if (num_calls > 0) {
+            Debug.Log("Update - enter");
+            num_calls = num_calls - 1;
+            AddToScore(100);
+            Debug.Log("Update - exit");
+        }
     }
 
     public void AddToScore(int scoreToBeAdded){
+        Debug.Log("AddToScore - enter");
+
+
         if (scoreToBeAdded > 0){
             m_Score += scoreToBeAdded;
+            //CurrentScore = transform.Find("Score");
+            scoreContainer.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
         }
+        Debug.Log("AddToScore - current score is " + m_Score.ToString());
+        Debug.Log("AddToScore - exit");
 
-        Debug.Log(CurrentScore.name);
-
-        // CurrentScore = transform.Find("CurrentScore");
-        // CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
     }
 }
