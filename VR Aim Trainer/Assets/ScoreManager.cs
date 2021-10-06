4dd5ae9009873ce8efe2c42cd143ac45168c3df3
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-06 15:07:29 -0700

    Revert "Switch scoremanager to static class for testclient"
    This reverts commit 2687ecf0ac2fd7a839256b01da8ce1c82fc61785.

--- VR Aim Trainer/Assets/ScoreManager.cs
+++ VR Aim Trainer/Assets/ScoreManager.cs
@@ -1,33 +1,30 @@
 using UnityEngine;
 using System;
 
-public static class ScoreManager { 
-    public static int m_Score = 0;
-    //public static GameObject CurrentScore;
+public class ScoreManager : MonoBehaviour
+{
+    private int m_Score;
+    private GameObject CurrentScore;
 
-    public static PlayerScore score;
-
-    //void static Start(){
-    //    //Debug.Log("Start - enter");
-    //    CurrentScore = GameObject.Find("Score");
-    //    // initialize scoreboard to 0
-    //    m_Score = 0;
-    //    CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
-    //    //Debug.Log("Start - exit");
-    //}
+    void Start(){
+        //Debug.Log("Start - enter");
+        CurrentScore = GameObject.Find("Score");
+        // initialize scoreboard to 0
+        m_Score = 0;
+        CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
+        //Debug.Log("Start - exit");
+    }
 
    
 
-    public static void AddToScore(int scoreToBeAdded){
-        Debug.Log("AddToScore - enter");
+    public void AddToScore(int scoreToBeAdded){
+        //Debug.Log("AddToScore - enter");
         if (scoreToBeAdded > 0){
-            GameObject CurrentScore;
             m_Score += scoreToBeAdded;
-            CurrentScore = GameObject.Find("Score");
             CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
         }
-        Debug.Log("AddToScore - current score is " + m_Score.ToString());
-        Debug.Log("AddToScore - exit");
+        //Debug.Log("AddToScore - current score is " + m_Score.ToString());
+        //Debug.Log("AddToScore - exit");
 
     }
 }
