108b324904acfde8a962712264c54b0c97aaafcf
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-18 12:23:21 -0700

    Change class name from CountdownTimer to Timer

--- VR Aim Trainer/Assets/Greg/Timer.cs
+++ VR Aim Trainer/Assets/Greg/Timer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountdownTimer : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     bool active = true;
     float currentTime = 0f;
