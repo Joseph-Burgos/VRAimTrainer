25a6ff6b94b28d9717726b552ab0e1156b0da3ea
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-26 19:53:46 -0700

    Add function to count every shot registered.

--- VR Aim Trainer/Assets/ScoreManager.cs
+++ VR Aim Trainer/Assets/ScoreManager.cs
@@ -4,6 +4,7 @@ using System;
 public class ScoreManager : MonoBehaviour
 {
     private int m_Score;
+    private int m_Shots;
     private GameObject CurrentScore;
 
     void Start(){
@@ -11,6 +12,7 @@ public class ScoreManager : MonoBehaviour
         CurrentScore = GameObject.Find("Score");
         // initialize scoreboard to 0
         m_Score = 0;
+        m_Misses = 0;
         CurrentScore.GetComponent<TMPro.TextMeshPro>().text = Convert.ToString(m_Score);
         Debug.Log("AddToScore - current START score is " + CurrentScore);
         //Debug.Log("Start - exit");
@@ -28,6 +30,9 @@ public class ScoreManager : MonoBehaviour
         Debug.Log("AddToScore - current  OBJECT score is " + CurrentScore);
         Debug.Log("AddToScore - current score is " + m_Score.ToString());
         Debug.Log("AddToScore - exit");
+    }
 
+    public void AddToShots() {
+        m_Shots++;
     }
 }
