298a760622364ccf5f68c6bc1c73b44f6c2b2f71
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-28 12:09:08 -0700

    Rename classes and objects, add additional data members

--- VR Aim Trainer/Assets/Joe/SaveManager.cs
+++ VR Aim Trainer/Assets/Joe/SaveManager.cs
@@ -12,40 +12,41 @@ public static class SaveManager
     //file name with saved data
     public static string savefileName =  "MyData.txt";
     //create an empty list to add onto
-    public static List<PlayerScore> scores = new List<PlayerScore>();
-    public static PlayerScoreList playerScoreList = null;
+    public static List<PlayerScore> playerScoreList = null;
+    public static SavedDataObject savedDataObject = null;
 
     // Saves a playerscore to disk.
     public static void addScore(PlayerScore ss)
     {
-        //get saveDirectory of file
-        string dir = Application.dataPath + saveDirectory;
-        //check if exists
-        if (!Directory.Exists(dir))
-        {
-            Debug.Log("Save file does not exist, creating");
-            Directory.CreateDirectory(dir);
-        }
+        Debug.Log("Add to playerScores");
+        // //get saveDirectory of file
+        // string dir = Application.dataPath + saveDirectory;
+        // //check if exists
+        // if (!Directory.Exists(dir))
+        // {
+        //     Debug.Log("Save file does not exist, creating");
+        //     Directory.CreateDirectory(dir);
+        // }
 
-        //creates a new player score board
-        PlayerScoreList scoresList = new PlayerScoreList { scores = scores };
-        //load old PlayerScoreList
-        PlayerScoreList oldscoresList = Load();
+        // //creates a new player score board
+        // SavedDataObject playerScoresList = new SavedDataObject { playerScores = playerScores };
+        // //load old SavedDataObject
+        // SavedDataObject oldplayerScoresList = Load();
    
-        //check if old PlayerScoreList empty
-        if (oldscoresList != null )
-        {
-            //store the old PlayerScoreList into the current PlayerScoreList
-            scoresList = oldscoresList;
-        }
-        //add into list
-        scoresList.scores.Add(ss);
+        // //check if old SavedDataObject empty
+        // if (oldplayerScoresList != null )
+        // {
+        //     //store the old SavedDataObject into the current SavedDataObject
+        //     playerScoresList = oldplayerScoresList;
+        // }
+        // //add into list
+        // playerScoresList.playerScores.Add(ss);
 
-        //parse save object into json string format
-        string json = JsonUtility.ToJson(scoresList);
+        // //parse save object into json string format
+        // string json = JsonUtility.ToJson(playerScoresList);
 
-        //save
-        File.WriteAllText(dir + savefileName, json);
+        // //save
+        // File.WriteAllText(dir + savefileName, json);
     }
 
     public static void Load()
@@ -56,19 +57,20 @@ public static class SaveManager
         if (File.Exists(absDataPath))
         {
             string json = File.ReadAllText(absDataPath);
-            playerScoreList = JsonUtility.FromJson<PlayerScoreList>(json);
+            savedDataObject = JsonUtility.FromJson<SavedDataObject>(json);
+            playerScoreList = savedDataObject.playerScores;
         }
         else
         {
-            Debug.Log("SAVE FILE DOES NOT EXIST, RETUNRING NULL");
+            Debug.Log("SAVE FILE DOES NOT EXIST");
         }
     }
 
-    public static PlayerScoreList GetPlayerScoreList() { return playerScoreList; }
+    public static SavedDataObject GetSavedDataObject() { return savedDataObject; }
 }
 
-//create a class of scores so we can save it
-public class PlayerScoreList
+//create a class of playerScores so we can save it
+public class SavedDataObject
 {
-    public List<PlayerScore> scores;
+    public List<PlayerScore> playerScores;
 }
