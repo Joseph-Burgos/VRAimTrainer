e29a6f73411afd8c31fa267e6438e3ef92a96ee7
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-28 11:51:03 -0700

    Add fields to playerscore

--- VR Aim Trainer/Assets/Joe/PlayerScore.cs
+++ VR Aim Trainer/Assets/Joe/PlayerScore.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 //THIS IS A PLAYERSCORE CLASS. IT CAN BE USED TO CREATE A TEXT FILE
 
-//lets class be savable to a file
+// Used to record key aspects of a played game.
+// A player is assigned a score.
+// This class is used to structure data that will be written to and read from disk when calculating VisualFeedback.
 [System.Serializable]
 public class PlayerScore
 {
+    public string dateTime;
     public string gameMode;
     public int score;
-    public int time;
+    public int gameTime;
+    // TODO add fields for accuracy, etc.
 
     public override string ToString()
     {
-        return "GameMode: " + gameMode + "Score: " + score + "Time: " + time;
+        return "Date & time: " + dateTime + "GameMode: " + gameMode + "Score: " + score + "Time: " + gameTime;
     }
 }
