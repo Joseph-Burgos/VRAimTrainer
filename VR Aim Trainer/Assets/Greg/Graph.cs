8e37d70da1dbf26d6f0cf4698e5e50c0d7f3b6fd
greg-navarro <gregorylitmannavarro@gmail.com>
2021-11-02 13:58:23 -0700

    Work on the drawGrid function

--- VR Aim Trainer/Assets/Greg/Graph.cs
+++ VR Aim Trainer/Assets/Greg/Graph.cs
@@ -11,10 +11,10 @@ using UnityEngine.UI;
 public class Graph : MonoBehaviour {
     [SerializeField] private Sprite circleSprite; // node 
     // parent objects
-    private GameObject graphContainer;
     private RectTransform graphContainerTransform; 
     // References to graph elements
-    private LineRenderer horizontalGridMarkerTemplate;
+    private Transform horizontalGridMarkerTemplate;
+    // private LineRenderer horizontalGridMarkerTemplate;
 
     // data for generating graph
     private float xLength;
@@ -23,8 +23,8 @@ public class Graph : MonoBehaviour {
 
     private void Awake() {
         // Retrieve references to necessary game objects
-        graphContainer = transform.Find("GraphContainer");
-        graphContainerTransform = graphContainer.GetComponent<RectTransform>();
+        graphContainerTransform = transform.Find("GraphContainer").GetComponent<RectTransform>();
+        horizontalGridMarkerTemplate = graphContainerTransform.Find("HorizontalGridMarkerTemplate"); //.GetComponent<LineRenderer>();
         // horizontalGridMarkerTemplate = 
         // set values
         initializeGraphData();
@@ -35,6 +35,7 @@ public class Graph : MonoBehaviour {
 
     public void createGraph() {
         CreateCircle(new Vector2(0, 0));
+        drawGrid(5, 0);
     }
 
     private void initializeGraphData() {
@@ -42,8 +43,27 @@ public class Graph : MonoBehaviour {
         yLength = graphContainerTransform.sizeDelta.y;
     }
 
-    private void setDividers(int xDividers, int yDividers) {
-
+    private void drawGrid(int xGrids, int yGrids) {
+        // define distances between grids
+        float xGridDistance = (xGrids > 0) ? yLength / xGrids: 0;
+        float yGridDistance = (yGrids > 0) ? xLength / yGrids : 0;
+        // x
+        // for num of x grids
+        float currentYValue = 0;
+        for (int i = 0; i < xGrids; i++) {
+            // create new line from the template
+            Transform xGridTransform = Instantiate(horizontalGridMarkerTemplate, graphContainerTransform);
+            LineRenderer xGridLineRenderer = xGridTransform.GetComponent<LineRenderer>();
+            // make an array of 2 vector3 elements for endpoints of the line
+            var points = new Vector3[2];
+            points[0] = new Vector3(0, currentYValue, 0);
+            points[1] = new Vector3(xLength, currentYValue, 0);
+            xGridLineRenderer.SetPositions(points);
+            xGridTransform.gameObject.SetActive(false); // set as active
+            currentYValue += xGridDistance;
+        }
+        // height = previous height + (i * yLength / xGrids)
+        // set the parent and draw the divider line
     }
 
     private GameObject CreateCircle(Vector2 anchoredPosition) {
