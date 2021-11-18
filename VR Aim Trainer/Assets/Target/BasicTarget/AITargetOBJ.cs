db4d56bad47efcd0254ff98f6be52cf868cf95f9
Donovan Lee <donovan10599@gmail.com>
2021-11-17 22:35:46 -0800

    trying a different tracking method

---
+++ VR Aim Trainer/Assets/Target/BasicTarget/AITargetOBJ.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public class OnHitEvent : UnityEvent<int>
+{
+}
+
+
+//object spawns, waits 2 seconds, and invokes addTarget which adds itself to a list of the game manager and destroys itself
+public class AITargetOBJ : Target_Parent
+{
+    public OnHitEvent onHit;
+    int counter = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if timer should be recording
+        if (timerActive)
+        {
+            //update the time that the target has been alive   
+            time += Time.deltaTime;
+            //Debug.Log(time);
+        }
+
+    }
+
+
+
+    //if hit by player weapon, add the target
+public override void hit()
+    {
+        //stop timer from recording
+        timerActive = false;
+        // Debug.Log("time: " + this.time);
+        //add target to list
+        //TargetManager.addTarget(this);
+
+        //counter incremented for how many time player hits target
+        counter++;
+        //play audio
+        //FindObjectOfType<AudioManager>().Play("MetalHit 1");
+        //AudioSource.PlayClipAtPoint(hitSound, this.transform.position);
+
+
+        //create a particle
+        GameObject burstObject = Instantiate(burst, this.transform.position, Quaternion.identity);
+        //destroy particle upon complete
+        //Destroy(burstObject, 1f);
+
+        onHit.Invoke(100);
+    }
+
+
+
+    ////when target is no longer needed, add to array in target  manager for score
+    //protected void addTarget()
+    //{
+    //    //add target to list of targets hit
+    //    //Debug.Log(this.time);
+    //    TargetManager.targets.Add(this);
+    //    //update the max amount of targets on screen
+    //    TargetManager.currentTargets = TargetManager.currentTargets - 1;
+    //    //destroy object
+    //    Destroy(this.gameObject);
+    //}
+}
